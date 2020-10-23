2891efc61c011d53c5caa90d8765ef1260438f09
PercyJackson-s <773811406@qq.com>
2020-10-23 11:24:01 +0800

    20201023commit

--- createbyidea/test1.java
+++ createbyidea/test1.java
@@ -1,7 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("test111");
-        System.out.println("test33");
         System.out.println("test222");
     }
 }
