d08f614d01e8cad460649b873ac4ca8d1b649cca
PercyJackson-s <58646336+PercyJackson-s@users.noreply.github.com>
2020-10-23 11:23:22 +0800

    Update test1.java

--- createbyidea/test1.java
+++ createbyidea/test1.java
@@ -1,6 +1,6 @@
 public class test1 {
     public static void main(String[] args) {
-        System.out.println("test111");
+        System.out.println("test11");
         System.out.println("test33");
     }
 }
